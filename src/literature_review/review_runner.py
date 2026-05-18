5d19bbb66b89a043a6e3cc2fae064f8366506281
ebeharry <emmabeharry@gmail.com>
2026-05-17 21:32:54 -0700

    add max papers as a parameter to run

--- src/literature_review/review_runner.py
+++ src/literature_review/review_runner.py
@@ -2,6 +2,6 @@ from src.literature_review.clients import search_semantic_scholar
 from src.literature_review.data_classes import Paper
 
 
-def run(project_description: str) -> list[Paper]:
-    papers = search_semantic_scholar(project_description)
+def run(project_description: str, max_papers: int = 20) -> list[Paper]:
+    papers = search_semantic_scholar(project_description, limit=max_papers)
     return papers
