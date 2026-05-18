c1bcc8931b7fb9b1ac7798edeb75c3d0adde0eb3
ebeharry <emmabeharry@gmail.com>
2026-05-17 21:05:45 -0700

    return semantic scholar papers

--- src/literature_review/review_runner.py
+++ src/literature_review/review_runner.py
@@ -1,11 +1,7 @@
-from src.literature_review.clients import search_arxiv, search_semantic_scholar
+from src.literature_review.clients import search_semantic_scholar
 from src.literature_review.data_classes import Paper
-from paperpath.shared.models import Cluster
 
 
-def run(project_description: str) -> list[Cluster]:
-    papers: list[Paper] = (
-        search_semantic_scholar(project_description)
-        + search_arxiv(project_description)
-    )
-    raise NotImplementedError  # clustering to be implemented
+def run(project_description: str) -> list[Paper]:
+    papers = search_semantic_scholar(project_description)
+    return papers
